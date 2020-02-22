b67f3219020818b7be0ff4c38e8a4fea63142a76
Birte Spreuer <mail@birtespreuer.com>
2020-02-22 17:26:10 +0100

    2. commit

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,43 +1,20 @@
 import React from "react";
 
-import { Link } from "react-router-dom";
-
 const Articles = props => (
   <div className="container">
     <div className="row">
       {props.articles.map(article => {
         return (
-          <div
-            key={article.title}
-            className="col-md-4"
-            style={{ marginBottom: "2rem" }}
-          >
+          <div key={article.title} className="col-md-6 col-sm-12 col-lg-4">
             <div className="article__box">
               <img
                 className="article__box-img"
-                src={article.image_url}
+                src={article.urlToImage}
                 alt={article.title}
               />
               <div className="article__text">
-                <h5 className="article__title">
-                  {article.title.length < 20
-                    ? `${article.title}`
-                    : `${article.title.substring(0, 25)}...`}
-                </h5>
-                <p className="article__subtitle">
-                  Publisher: <span>{recipe.publisher}</span>
-                </p>
+                <h5>{article.title} </h5>
               </div>
-              <button className="recipe_buttons">
-                <Link
-                  to={{
-                    pathname: `/recipe/${recipe.recipe_id}`,
-                    state: { recipe: recipe.title }
-                  }}
-                >
-                  View Recipe
-                </Link>
-              </button>
             </div>
           </div>
         );
@@ -46,4 +23,4 @@ const Articles = props => (
   </div>
 );
 
-export default Recipes;
+export default Articles;
